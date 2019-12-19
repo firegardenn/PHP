c430340f3fca0f33e651d1989fb51b5039912bb4
Cristian Cominelli <firegarden1977@gmail.com>
2019-12-19 22:28:57 +0100

    first commit

---
+++ PHP/Dizionario.php
@@ -0,0 +1,51 @@
+<html>
+<body>
+
+<?php
+$string=file_get_contents('Dizionario.json');
+$decode = json_decode($string, true);
+$name=$_POST["newParola"];
+$name2=$_GET["parola"];
+$delete=$_POST["delete"];
+$check=1;
+if($name2!=null){
+    cerca();
+}
+if($delete!=null){
+    cancella();
+}
+function cerca(){
+    Global $decode, $name2, $check;
+    foreach ($decode as $key => $value) {
+        if($key!=$name2){
+            $check = 1;
+        }
+        else{
+            $check = 0;
+            echo $key.": ".$value;
+            break;
+        }
+    }
+    if($check==1){
+        echo "parola non trovata";
+    }
+}
+function cancella(){
+    Global $decode, $delete;
+    foreach ($decode as $key=>$value) {
+        if($delete == $key){
+            unset($decode[$delete]);
+            echo ("elemento cancellato");
+         }
+    }
+}
+
+$definizione=$_POST["definizione"];
+$decode[$name]=$definizione;
+$decode=json_encode($decode, true);
+
+file_put_contents("Dizionario.json", $decode);
+
+?>
+</body>
+</html>
